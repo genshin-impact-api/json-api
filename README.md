e07f2188f1e073fb5b42a2d14c3ecb60f950c873
Jack <jack20191124@proton.me>
2022-10-08 13:02:23 +0800

    Load character data

---
+++ README.md
@@ -0,0 +1,1 @@
+https://my-json-server.typicode.com/genshin-impact-api/json-api
--- sync.py
+++ sync.py
@@ -1,17 +1,24 @@
+import base64
+from collections import OrderedDict
+
+import requests
+import json
+
+PLAYABLE_CHARACTERS_DATA_URL\
+    = "https://api.github.com/repos/genshin-impact-api/wiki-data/contents/playable-characters.json"
+
 if __name__ == "__main__":
-    with open("db.json", "w") as data_file:
-        data_file.write(
-            """
-            {
-                "posts": [
-                    {
-                        "id": 1,
-                        "title": "hello"
-                    }
-                ],
-                "profile": {
-                    "name": "typicode"
-                }
-            }
-            """
+    data: dict = OrderedDict()
+
+    playable_characters: list[dict] = json.loads(
+        base64.b64decode(json.loads(requests.get(PLAYABLE_CHARACTERS_DATA_URL).text)["content"]).decode('utf-8')
+    )
+    data["playableCharacters"] = []
+    for idx in range(len(playable_characters)):
+        playable_characters[idx]["id"] = idx + 1
+        data["playableCharacters"].append(
+            playable_characters[idx]
         )
+
+    with open("db.json", "w") as data_file:
+        data_file.write(json.dumps(data, indent=4, ensure_ascii=False))
