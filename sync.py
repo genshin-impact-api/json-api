8abb2030cf999303f98f5fabc54c44784ee3a933
Jack <jack20191124@proton.me>
2022-10-08 12:35:30 +0800

    [Sanity Check] My JSON Server

--- sync.py
+++ sync.py
@@ -1,3 +1,17 @@
 if __name__ == "__main__":
     with open("db.json", "w") as data_file:
-        data_file.write("First data")
+        data_file.write(
+            """
+            {
+                "posts": [
+                    {
+                        "id": 1,
+                        "title": "hello"
+                    }
+                ],
+                "profile": {
+                    "name": "typicode"
+                }
+            }
+            """
+        )
